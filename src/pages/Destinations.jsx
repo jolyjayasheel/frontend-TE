5e24bd8e69ff7b7628c735ab9a9969a020543404
Jolly Jayasheel <jolyjayasheel@gmail.com>
2026-06-27 01:52:35 +0530

    Fix accidental embedded repository

---
+++ src/pages/Destinations.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import DestinationCard from "../components/DestinationCard";
+import { Link } from "react-router-dom";
+
+function Destinations() {
+ const [destinations, setDestinations] =
+   useState([]);
+
+ useEffect(() => {
+   getDestinations();
+ }, []);
+
+ async function getDestinations() {
+   try {
+     const response = await api.get(
+       "/destinations"
+     );
+
+     setDestinations(response.data);
+   } catch (error) {
+     console.log(error);
+   }
+ }
+ async function deleteDestination(id) {
+
+ await api.delete(
+   `/destinations/${id}`
+ );
+
+ setDestinations(
+   destinations.filter(
+     destination =>
+     destination.id !== id
+   )
+ );
+}
+
+ return (
+   <>
+     <h1>Popular Destinations</h1>
+
+     <Link to="/add-destination" className="add-btn">
+ Add Destination
+</Link>
+
+     <div className="destinations">
+       {destinations.length>0? (destinations.map((destination) => (
+         <DestinationCard
+           key={destination.id}
+           destination={destination}
+           onDelete={deleteDestination} 
+         />
+       ))):<h1>Loading...</h1>}
+     </div>
+   </>
+ );
+}
+
+export default Destinations;
